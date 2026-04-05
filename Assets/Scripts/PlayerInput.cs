91ef3b1d79fd3ee771f09a6a205db953df24e439
Yanxxx <060509cindy@gmail.com>
2026-04-05 19:15:29 -0400

    Added assets, player input movement

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
+    public float speed = 5f;
+    public Vector2 movement;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,11 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += (Vector3)movement * Time.deltaTime * speed;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
     }
 }
