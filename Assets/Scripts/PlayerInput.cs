fb787de012e1cd40906709ca24adcd443dc2365d
Yanxxx <060509cindy@gmail.com>
2026-04-05 20:52:16 -0400

    feat: Check use tool input

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,6 +5,7 @@ public class PlayerInput : MonoBehaviour
 {
     public float speed = 5f;
     public Vector2 movement;
+    public bool isUsingTool = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,11 +15,19 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Move the player based on the movement vector and speed
         transform.position += (Vector3)movement * Time.deltaTime * speed;
     }
 
+    //When the player presses the movement keys, update the movement vector
     public void OnMove(InputAction.CallbackContext context)
     {
         movement = context.ReadValue<Vector2>();
+    }
+
+    public void OnUseTool(InputAction.CallbackContext context)
+    {
+        isUsingTool = context.ReadValueAsButton();
+        Debug.Log(isUsingTool);
     }
 }
